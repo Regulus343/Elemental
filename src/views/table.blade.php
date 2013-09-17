d703d2bfcd3cd0e66fca81fc0e69c38110219c4d
Regulus343 <codyjassman@gmail.com>
2013-09-16 20:04:24 -0600

    updated table() to include 'no data' row if data not present

--- src/views/table.blade.php
+++ src/views/table.blade.php
@@ -10,44 +10,52 @@
 		</tr>
 	</thead>
 	<tbody>
-		@foreach ($data as $dataRow)
-			<tr id="{{ $rows['idPrefix'].$dataRow->id }}"{{ Elemental::getTableRowClass($dataRow, $rows) }}>
-				<?php $dataRowArray = $dataRow->toArray(); ?>
-				@foreach ($columns as $column)
-					@if (!$column['developer'] || Session::get('developer'))
-						@if (isset($column['method']))
+		@if (count($data))
+			@foreach ($data as $dataRow)
+				<tr id="{{ $rows['idPrefix'].$dataRow->id }}"{{ Elemental::getTableRowClass($dataRow, $rows) }}>
+					<?php $dataRowArray = $dataRow->toArray(); ?>
+					@foreach ($columns as $column)
+						@if (!$column['developer'] || Session::get('developer'))
+							@if (isset($column['method']))
 
-							@if (isset($column['attribute']) && $column['type'] == "list")
-								<td>{{ Format::objListToStr($dataRow->$column['method'], $column['attribute']) }}</td>
-							@else
-								<td>{{ $dataRow->$column['method']() }}</td>
-							@endif
-
-						@elseif (isset($column['attribute']))
-							@foreach ($dataRowArray as $dataCol => $dataCell)
-								@if ($dataCol == $column['attribute'])
-
-									<td>{{ Elemental::formatTableCellData($dataCell, $column['type']) }}</td>
-
-								@endif
-							@endforeach
-						@else
-							<td>
-								@if (isset($column['elements']) && !empty($column['elements']))
-									@foreach ($column['elements'] as $element)
-
-										{{ Elemental::createElement($element, $dataRowArray) }}
-
-									@endforeach
+								@if (isset($column['attribute']) && $column['type'] == "list")
+									<td>{{ Format::objListToStr($dataRow->$column['method'], $column['attribute']) }}</td>
 								@else
-									&nbsp;
+									<td>{{ $dataRow->$column['method']() }}</td>
 								@endif
-							</td>
+
+							@elseif (isset($column['attribute']))
+								@foreach ($dataRowArray as $dataCol => $dataCell)
+									@if ($dataCol == $column['attribute'])
+
+										<td>{{ Elemental::formatTableCellData($dataCell, $column['type']) }}</td>
+
+									@endif
+								@endforeach
+							@else
+								<td>
+									@if (isset($column['elements']) && !empty($column['elements']))
+										@foreach ($column['elements'] as $element)
+
+											{{ Elemental::createElement($element, $dataRowArray) }}
+
+										@endforeach
+									@else
+										&nbsp;
+									@endif
+								</td>
+							@endif
 						@endif
-					@endif
-				@endforeach
+					@endforeach
+				</tr>
+			@endforeach
+		@else
+			<tr>
+				<td colspan="{{ count($columns) }}" class="no-data">
+					{{ isset($table['noDataMessage']) ? $table['noDataMessage'] : '' }}
+				</td>
 			</tr>
-		@endforeach
+		@endif
 	</tbody>
 	@if ($footer)
 		<tfoot>
